ede3eeb67fba725fc437be92398db4afe56f7f62
Matteo D'amico <matt.dmc08@gmail.com>
2020-09-08 13:47:31 +0000

    changed init-script, disabling the unused ansible-role nickjj.docker

--- init-script.sh
+++ init-script.sh
@@ -7,7 +7,8 @@ pushd ansible
 pip install ansible 
 pip install requests google-auth
 
-ansible-galaxy install nickjj.docker
+# you can use the follow ansible-role if you are running in Ubuntu OS
+# ansible-galaxy install nickjj.docker
 ansible-galaxy install nickjj.user
 
 popd 
@@ -15,4 +16,4 @@ popd
 source part0_source_creds.sh
 
 cp role_user_vars.yml ansible/roles/nickjj.user/defaults/main.yml
-cp role_docker_vars.yml ansible/role/nickjj.docker/defaults/main.yml
+# cp role_docker_vars.yml ansible/role/nickjj.docker/defaults/main.yml
