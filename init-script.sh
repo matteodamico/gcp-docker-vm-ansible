052c02bd0380e2788f190996ae0b63655d9d16d7
Matteo D'amico <matt.dmc08@gmail.com>
2020-09-08 10:46:31 +0000

    added user Docker to VM; changed ansible role to install docker cause nickjj.docker doesn't support CentOS

--- init-script.sh
+++ init-script.sh
@@ -7,10 +7,12 @@ pushd ansible
 pip install ansible 
 pip install requests google-auth
 
-#sudo apt install ansible --assume-yes
-
-sudo ansible-galaxy install nickjj.docker
+ansible-galaxy install nickjj.docker
+ansible-galaxy install nickjj.user
 
 popd 
 
 source part0_source_creds.sh
+
+cp role_user_vars.yml ansible/roles/nickjj.user/defaults/main.yml
+cp role_docker_vars.yml ansible/role/nickjj.docker/defaults/main.yml
--- run-playbook.sh
+++ run-playbook.sh
@@ -2,7 +2,7 @@
 
 cd ansible;
 
-echo "Test variabile GCP_SERVICE_ACCOUNT_EMAIL:"
-echo $GCP_SERVICE_ACCOUNT_EMAIL
+#echo "Test variabile GCP_SERVICE_ACCOUNT_EMAIL:"
+#echo $GCP_SERVICE_ACCOUNT_EMAIL
 
 ansible-playbook playbooks/playbook.yml -vvv -i inventory;
