c26ab794e59e1f95880bc4dad764ce1d0f52aae2
Matteo D'amico <matt.dmc08@gmail.com>
2020-09-09 08:03:34 +0000

    removed unused files, rebase to provide only VMs and docker

--- init-script.sh
+++ init-script.sh
@@ -6,15 +6,10 @@ pushd ansible
 
 pip install ansible 
 pip install requests google-auth
-pip install tox
-sudo apt-get install -y python3-jmespath
-
 ansible-galaxy install nickjj.user
-ansible-galaxy install atosatto.docker-swarm
 
 popd 
 
 source part0_source_creds.sh
 
 cp role_user_vars.yml ansible/roles/nickjj.user/defaults/main.yml
-cp role_docker_vars.yml ansible/role/nickjj.docker/defaults/main.yml
