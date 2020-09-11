f9317a0c957d3a6fba1afde819b19c0b95c51a5d
Matteo D'amico <matt.dmc08@gmail.com>
2020-09-11 13:18:32 +0000

    add jmespath in init-scriptwq!

--- init-script.sh
+++ init-script.sh
@@ -7,6 +7,7 @@ sudo apt install python-pip -y
 sudo apt install software-properties-common -y
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install ansible -y
+sudo apt-get install -y python-jmespath
 
 pushd ansible
 
